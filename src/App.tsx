fb9291749e331f3e2396691dab29fcc912fa350d
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-07-21 10:21:20 +0200

    style: changed name of app

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ function App() {
       <>
         <Toast />
         <div>
-          <h1 className='text-5xl font-mono mb-3'> text2book </h1>
+          <h1 className='text-5xl font-mono mb-3'> Text2Book </h1>
           <p className='mb-3 text-md'> Text to Minecraft book generator </p>
         </div>
       </>

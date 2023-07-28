6a74a40e50adc00b366aa9a1564f35a768f69f49
thewilley <89783791+TheWilley@users.noreply.github.com>
2023-07-28 10:43:28 +0200

    feat: added ability to upload files

---
+++ src/components/FileUploadSingle.tsx
@@ -0,0 +1,49 @@
+import { ChangeEvent, useState } from 'react';
+
+function FileUploadSingle(props: { callback: (text: string) => void }) {
+    const [fileName, setFileName] = useState<string>('');
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.item(0);
+
+        // If file type is text/plain
+        if (file?.type === 'text/plain') {
+            // Read the file
+            const reader = new FileReader();
+            reader.readAsText(file);
+
+            // Set the file name
+            setFileName(file.name);
+
+            // Set the text
+            reader.onload = () => {
+                props.callback(reader.result as string);
+            };
+        }
+    };
+
+    return (
+        <div>
+            <div className="relative mb-3">
+                <input
+                    id="file-upload"
+                    type="file"
+                    onChange={handleFileChange}
+                    accept='.txt'
+                    className="opacity-0 absolute z-0 w-full h-full"
+                />
+                <div className="bg-white rounded-md border border-gray-400 px-4 py-2 flex items-center justify-between">
+                    <span className="text-gray-700">{fileName || 'Choose a file'}</span>
+                    <button
+                        type="button"
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:cursor-pointer"
+                    >
+                        Browse
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FileUploadSingle;
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import loader from '../assets/loader.svg';
+import FileUploadSingle from './FileUploadSingle';
 
 function Form(props: { callback: (text: string, author: string, title: string) => void }) {
     // Normal states
@@ -7,6 +8,8 @@ function Form(props: { callback: (text: string, author: string, title: string) =
     const [author, setAuthor] = useState('');
     const [title, setTitle] = useState('');
     const [loading, setLoading] = useState(false);
+    const [useFileUpload, setUseFileUpload] = useState(false);
+    const [rawOutput, setRawOutput] = useState(false);
 
     // Handle the submit
     const handleSubmit = (event: React.FormEvent) => {
@@ -56,66 +59,88 @@ function Form(props: { callback: (text: string, author: string, title: string) =
 
     useEffect(() => {
         setText(localStorage.getItem('text') || '');
-        setAuthor(localStorage.getItem('author') ||'');
-        setTitle(localStorage.getItem('title') ||'');
+        setAuthor(localStorage.getItem('author') || '');
+        setTitle(localStorage.getItem('title') || '');
     }, []);
 
     return (
-        <form
-            onSubmit={handleSubmit}
-        >
-            <div className="flex flex-wrap mb-4">
-                <div className="w-full sm:w-1/2 px-2">
-                    <label htmlFor="author" className="block text-gray-700 text-sm font-bold mb-2">
-                        Author
-                    </label>
-                    <input
-                        type="text"
-                        id="author"
-                        placeholder='Lewis Carroll'
-                        className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
-                        value={author}
-                        onChange={handleChanngeAuthor}
-                        maxLength={50}
-                        required
-                    />
-                </div>
-                <div className="w-full sm:w-1/2 px-2">
-                    <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">
-                        Title
-                    </label>
-                    <input
-                        type="text"
-                        id="title"
-                        placeholder='Alice in Wonderland'
-                        className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
-                        value={title}
-                        onChange={handleChangeTitle}
-                        maxLength={15}
-                        required
-                    />
+        <>
+            <div className="flex flex-col items-center">
+                <div className="border rounded-md p-4 mb-4">
+                    <div className="mb-4 flex items-center">
+                        <input id="use-file-upload" type="checkbox" className="mr-2 leading-tight h-4 w-4" onChange={() => setUseFileUpload(!useFileUpload)} />
+                        <label htmlFor="use-file-upload" className="text-gray-700">
+                            Use file upload
+                        </label>
+                    </div>
+                    <div className="flex items-center">
+                        <input id="raw-output" type="checkbox" className="mr-2 leading-tight p-2 h-4 w-4" onChange={() => setRawOutput(!rawOutput)} />
+                        <label htmlFor="raw-output" className="text-gray-700">
+                            Generate raw text
+                        </label>
+                    </div>
                 </div>
             </div>
-            <div className="mb-4">
-                <label htmlFor="text" className="block text-gray-700 text-sm font-bold mb-2">
-                    Text
-                </label>
-                <textarea
-                    id="text"
-                    placeholder='Once upon a time, there was a girl...'
-                    className="w-full h-72 border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300 h-32 resize-none"
-                    value={text}
-                    onChange={handleChangeText}
-                    required
-                />
-            </div>
-            <button
-                type="submit"
-                className="w-full h-10 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            <form
+                onSubmit={handleSubmit}
             >
-                {getButtonTitle()}
-            </button>
-        </form>
+                <div className="flex flex-wrap mb-4">
+                    <div className="w-full sm:w-1/2 px-2">
+                        <label htmlFor="author" className="block text-gray-700 text-sm font-bold mb-2">
+                            Author
+                        </label>
+                        <input
+                            type="text"
+                            id="author"
+                            placeholder='Lewis Carroll'
+                            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
+                            value={author}
+                            onChange={handleChanngeAuthor}
+                            maxLength={50}
+                            required
+                        />
+                    </div>
+                    <div className="w-full sm:w-1/2 px-2">
+                        <label htmlFor="title" className="block text-gray-700 text-sm font-bold mb-2">
+                            Title
+                        </label>
+                        <input
+                            type="text"
+                            id="title"
+                            placeholder='Alice in Wonderland'
+                            className="w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300"
+                            value={title}
+                            onChange={handleChangeTitle}
+                            maxLength={15}
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="text" className="block text-gray-700 text-sm font-bold mb-2">
+                        Text
+                    </label>
+                    <div>
+                        {useFileUpload ? <FileUploadSingle callback={(text) => setText(text)} /> :
+                            <textarea
+                                id="text"
+                                placeholder='Once upon a time, there was a girl...'
+                                className="w-full h-72 border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:border-blue-300 h-32 resize-none"
+                                value={text}
+                                onChange={handleChangeText}
+                                required
+                            />
+                        }
+                    </div>
+                </div>
+                <button
+                    type="submit"
+                    className="w-full h-10 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                    {getButtonTitle()}
+                </button>
+            </form>
+        </>
     );
 }
 

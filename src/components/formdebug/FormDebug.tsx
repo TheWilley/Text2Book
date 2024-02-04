1522982c243955a41668c6a8c40cd38ada076318
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-02-04 23:22:56 +0100

    fix: fixed bug where debug page would refuse to load

--- src/components/formdebug/FormDebug.tsx
+++ src/components/formdebug/FormDebug.tsx
@@ -1,14 +1,17 @@
 import { IShowResults } from '../../global/types.ts';
 import useForm from '../../hooks/useForm.ts';
 import FormDebugInput from './FormDebugInput.tsx';
+import { useEffect } from 'react';
 
 function FormDebug(props: { showResults: IShowResults }) {
   const { text, setText, setOutputFormat, loading, handleSubmit } = useForm(
     props.showResults
   );
 
-  // Set output format to text as we don't want to generate a command
-  setOutputFormat('text');
+  useEffect(() => {
+    // Set output format to text as we don't want to generate a command
+    setOutputFormat('text');
+  }, []);
 
   return (
     <>

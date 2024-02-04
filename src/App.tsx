67f24cb3bf344e11580deda5d84aad6ecf99af2d
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-02-04 22:31:12 +0100

    feat: changed to hashrouter to support github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import 'react-toastify/dist/ReactToastify.css';
 import {
-  createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
+  createHashRouter,
 } from 'react-router-dom';
 import Layout from './pages/Layout.tsx';
 import Home from './pages/Home.tsx';
 import Debug from './pages/Debug.tsx';
 import Error from './pages/Error.tsx';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />} errorElement={<Error />}>
       <Route index element={<Home />} />
       <Route path='/debug' element={<Debug />} />
     </Route>
-  ),
-  { basename: '/Text2Book' }
+  )
 );
 
 function App() {

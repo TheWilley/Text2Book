5968aeba932930400be550c1610ed580b113313b
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-03-28 14:15:50 +0100

    feat: updated warning message in banner

--- src/layout/Banner.tsx
+++ src/layout/Banner.tsx
@@ -7,7 +7,7 @@ function Banner(props: Props) {
     props.visisble && (
       <div className='w-full bg-red-300 rounded-md p-2'>
         <span>
-          ⚠️ Set number of lines to <u>13</u> if words get cut off
+          ⚠️ Set number of lines to <u>13</u> or below if words get cut off
         </span>
       </div>
     )

a846872ccdf80805c5c427b5cf7a95103df8f814
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-02-09 16:25:27 +0100

    feat: changelog is now a table instead of a list

--- src/views/Changelog.tsx
+++ src/views/Changelog.tsx
@@ -5,41 +5,45 @@ function Changelog() {
   return (
     <div className='mb-2'>
       <Accordion label='📜 Changelog' id='changelog'>
-        <ul>
-          <li className='text-left w-full space-y-5'>
-            {changelog.map((log, index) => (
-              <div key={index}>
-                <h3 className='font-bold underline mb-2'>{log.date}</h3>
-                <ul className='space-y-3'>
-                  {log.features.map((feature, index) => (
-                    <li className='flex gap-2' key={index}>
-                      <span>
-                        <span className='bg-purple-400 rounded-full px-2 py-1 font-mono'>
-                          New
-                        </span>
-                      </span>
-                      <span> {feature}</span>
-                    </li>
-                  ))}
-                </ul>
-                <ul className='space-y-3 mt-3'>
-                  {log.fixes.map((fix, index) => (
-                    <li className='flex gap-2' key={index}>
-                      <span>
-                        <span className='bg-blue-400 rounded-full px-2 py-1 font-mono'>
-                          Fix
-                        </span>
-                      </span>
-                      <span> {fix}</span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+        <table className='w-full border-collapse bg-white rounded-xl border-blue-200'>
+          <thead>
+            <tr>
+              <th className='text-left p-2 border'>
+                <span className='bg-purple-700 rounded-lg text-white p-1'>Date</span>
+              </th>
+              <th className='text-left p-2 border'>
+                <span className='bg-green-700 rounded-lg text-white p-1'>Features</span>
+              </th>
+              <th className='text-left p-2 border'>
+                <span className='bg-black rounded-lg text-white p-1'>Fixes</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody className='text-left border'>
+            {changelog.map((log, logIndex) => (
+              <tr key={logIndex}>
+                <td className='p-2 align-top border'>
+                  <h3 className='mb-2'>{log.date}</h3>
+                </td>
+                <td className='p-2 align-top border'>
+                  <ul className='list-disc list-inside'>
+                    {log.features.map((feature, featureIndex) => (
+                      <li key={featureIndex}>{feature}</li>
+                    ))}
+                  </ul>
+                </td>
+                <td className='p-2 align-top border'>
+                  <ul className='list-disc list-inside'>
+                    {log.fixes.map((fix, fixIndex) => (
+                      <li key={fixIndex}>{fix}</li>
+                    ))}
+                  </ul>
+                </td>
+              </tr>
             ))}
-          </li>
-        </ul>
+          </tbody>
+        </table>
       </Accordion>
-      <hr className='my-4' />
     </div>
   );
 }

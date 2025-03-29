1d5ed0afa5329351808a5df7ba4e285219233169
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-03-29 16:12:16 +0100

    chore: resolved prettierrc and eslint conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
     root: true,
-    env: {browser: true, es2020: true},
+    env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
@@ -20,11 +20,10 @@ module.exports = {
     rules: {
         'react-refresh/only-export-components': [
             'warn',
-            {allowConstantExport: true},
+            { allowConstantExport: true },
         ],
         '@typescript-eslint/no-non-null-assertion': 'off',
         semi: 2,
-        quotes: [1, 'single'],
         'no-console': 2,
     },
 };
